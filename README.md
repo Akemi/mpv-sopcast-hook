1a91bc2c5a389963a5e68dd2c6701c3991845ead
Akemi <der.richter@gmx.de>
2017-01-23 19:16:28 +0100

    add luasocket support

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Usage
 -----
 1. Move this script to ~/.config/mpv/scripts
 2. Make sure sp-sc-auth/sp-sc is in your $PATH or in ~/.config/mpv
+3. Install [luasocket](https://github.com/diegonehab/luasocket)
--- sopcast.lua
+++ sopcast.lua
@@ -3,19 +3,21 @@
 -- How to use this script:
 -- 1. Move this script to ~/.config/mpv/scripts
 -- 2. Make sure sp-sc-auth/sp-sc is in your $PATH or in ~/.config/mpv
+-- 3. Install luasocket
 
 local utils = require 'mp.utils'
 local msg = require 'mp.msg'
+local socket = require 'socket'
 
 local vars = {
     sopcast = "sp-sc-auth",
     sopcast_names = {"sp-sc-auth", "sp-sc"},
     sopcast_args = "",
-    sopcast_running = false,
+    sock = nil,
     url = "",
-    process_id = "",
     port = 8902,
-    wait_time = 5
+    timeout = 10,
+    debug = false
 }
 
 local function sleep(s)
@@ -31,7 +33,7 @@ end
 local function find_unused_port()
     local command = { "ps", "-o", "command" }
     local status, processes = exec(command)
-    
+
     local result = vars.port
     for i in string.gmatch(processes, "[^\r\n]+") do
         for j, name in ipairs(vars.sopcast_names) do
@@ -41,14 +43,29 @@ local function find_unused_port()
                     result = port+1
                 end
             end
-        end 
+        end
     end
     return result
 end
 
+local function get_info()
+    vars.sock:send("s\n")
+    local response, status, partial = vars.sock:receive()
+    info = {}
+    for ele in response:gmatch("%w+") do table.insert(info, ele) end
+    return info
+end
+
+function debug_on_tick(event)
+    if vars.sock == nil then return end
+    local info = get_info()
+    msg.warn("cache: " .. info[1] .. " ur: " .. info[2] .. " dr: " .. info[3] ..
+             " us: " .. info[4] .. " ds: " .. info[5] .. " peers: " .. info[6])
+end
+
 local function on_start()
     vars.url = mp.get_property("stream-open-filename")
-    
+
     if (vars.url:find("sop://") == 1) then
         -- find sopcast binary, search various names
         for i, name in ipairs(vars.sopcast_names) do
@@ -59,39 +76,48 @@ local function on_start()
                 break
             end
         end
-        
+
         -- find an unused port, needed for simultaneous streams
         vars.port = find_unused_port()
-        
+
         -- start sopcast
-        vars.sopcast_running = true
         msg.verbose("starting sopcast on port " .. vars.port)
         vars.sopcast_args = vars.url .. " 3908 " .. vars.port
         io.popen(vars.sopcast .. " " .. vars.sopcast_args .. " &")
-        
-        -- wait a few seconds, this is not nice but can't read output of sp-sc-auth
-        sleep(vars.wait_time)
-        
-        -- check if sopcast is running
-        local command = { "pgrep", "-f", vars.sopcast .. " " .. vars.sopcast_args }
-        local status, id = exec(command)
 
-        if (status < 0) or (id == nil) or (id == "") then
-            msg.warn("sopcast process not found. unresponsive or unuvailable channel.")
+        -- try up to vars.timeout times to connect to sopcast
+        local timeout = 0
+        while (vars.sock == nil and timeout < vars.timeout) do
+            timeout = timeout + 1
+            vars.sock = socket.connect("localhost", vars.port)
+            sleep(1)
+        end
+
+        -- check if connection worked
+        if vars.sock ~= nil then
+            -- print debug info on tick
+            if vars.debug then
+                -- prepare sopcast for cache status
+                vars.sock:settimeout(10)
+                assert(vars.sock:send("state\ns\n"))
+                mp.register_event("tick", debug_on_tick)
+            end
+
+            -- open the local sopcast stream
+            mp.set_property("stream-open-filename", "http://localhost:" .. vars.port)
+        else
+            msg.warn("could not connect to sopcast. unresponsive or unuvailable channel.")
             mp.command("quit")
             return
-        else
-            msg.verbose("sopcast process found. channel available.")
-            vars.process_id = id
         end
-        
-        -- open the local sopcast stream
-        mp.set_property("stream-open-filename", "http://localhost:" .. vars.port)
+
     end
 end
 
 function on_end(event)
-    if (vars.sopcast_running) then
+    if vars.sock ~= nil then
+        mp.unregister_event(tick)
+        vars.sock:close()
         os.execute("pkill -f \"".. vars.sopcast .. " " .. vars.sopcast_args .. "\"" )
         msg.verbose("sopcast terminated.")
     end
